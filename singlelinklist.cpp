18e2122696cfe9258b9a3aa42d081c0ee60f07f9
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-08 19:15:43 +0700

    menambahkan bool delNode

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -28,7 +28,7 @@ class LinkedList
 
         if (START == NULL || nim <= START -> noMhs)
         {
-            if ((START |= NULL) && (nim == START->noMhS))
+            if ((START != NULL) && (nim == START->noMhs))
             {
                 cout << "\nDuplikasi noMhs tidak diizinkan\n";
                 return;
@@ -40,7 +40,7 @@ class LinkedList
         Node *previous =START;
         Node *current = START;
 
-        while ((current |= NULL) && (nim >= current -> noMhs))
+        while ((current != NULL) && (nim >= current -> noMhs))
         {
             if (nim == current -> noMhs) 
             {
@@ -64,16 +64,25 @@ class LinkedList
         *previous = START;
         *current = START;
 
-        while ((*current |= NULL) && (nim |= (*current) ->noMhs))
+        while ((*current != NULL) && (nim != (*current) ->noMhs))
         {
             *previous = *current;
             *current = (*current) ->next;
         }
-        return (*current |= NULL);
+        return (*current != NULL);
     }
     bool delNode (int nim)
     {
         Node *current, *previous;
-        if (!Search (nim, previous &current) ) 
+        if (!Search (nim, &previous, &current) ) 
+        return false;
+
+        if (current == START)
+        START = START -> next;
+        else
+        previous -> next = current -> next;
+        
+        delete current;
+        return true;
     }
-}
+};
