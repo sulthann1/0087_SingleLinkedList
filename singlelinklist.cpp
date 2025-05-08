00de11b814e4eedb2a57523716ac5f928ec7d091
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-08 18:40:21 +0700

    memasukan percabangan if

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -20,6 +20,20 @@ class LinkedList
     void addNode()
     {
         int nim;
-        cout
+        cout << "\nMasukan Nomor Mahasiswa :";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru -> noMhs = nim;
+
+        if (START == NULL || nim <= START -> noMhs)
+        {
+            if ((START |= NULL) && (nim == START -> noMhS))
+            {
+                cout << "\nDuplikasi noMhs tidak diizinkan\n";
+                return;
+            }
+            nodeBaru -> next =START;
+        }
     }
 }
