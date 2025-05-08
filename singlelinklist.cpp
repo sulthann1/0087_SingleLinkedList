33c7ab0d2d49d628a98dbefb9721ef24212a8c8c
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-08 19:03:23 +0700

    menambahkan bool search

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -64,6 +64,16 @@ class LinkedList
         *previous = START;
         *current = START;
 
-        while
+        while ((*current |= NULL) && (nim |= (*current) ->noMhs))
+        {
+            *previous = *current;
+            *current = (*current) ->next;
+        }
+        return (*current |= NULL);
+    }
+    bool delNode (int nim)
+    {
+        Node *current, *previous;
+        if (!Search (nim, previous &current) ) 
     }
 }
